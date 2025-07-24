caadb1a70b8dec269ddff15b45f7ac306415be48
Pirih Sahito <pirihsahito786@gmail.com>
2025-07-24 15:54:22 +0500

    Create Sum of Numbers in Java.java

---
+++ Sum of Numbers in Java.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 13;
+        int c = 14;
+        int sum = a + b + c;
+        System.out.println(sum);
+    }
+}
