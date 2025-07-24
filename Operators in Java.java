675c0ed08534bff24c4d12e786c421ca255a35c1
Pirih Sahito <pirihsahito786@gmail.com>
2025-07-24 16:13:31 +0500

    Operators in Java.java

---
+++ Operators in Java.java
@@ -0,0 +1,39 @@
+public class Operators {
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 4;
+        // 1. Arithmetic Operators
+        System.out.println("Addition of a and b :");
+        System.out.println(a + b);
+        System.out.println("Subtraction of a and b :");
+        System.out.println(a - b);
+        System.out.println("Multiplication of a and b :");
+        System.out.println(a * b);
+        System.out.println("Division of a and b :");
+        System.out.println(a / b);
+        System.out.println("Remainder of a / b :");
+        System.out.println(a % b); // Module Operator
+        //System.out.println(4.8 % 1.1); // Returns Decimal Remainder
+
+        // 2. Assignment operators
+        //int b = 9;
+        //b = += 3;
+        //System.out.println(b);
+        //int b = 9;
+        //b = -= 3;
+        //System.out.println(b);
+        //int b = 9;
+        //b = *= 3;
+        //System.out.println(b);
+
+        // 3. Comparison Operators
+        //System.out.println(a == a);
+        //System.out.println(a == b);
+        //System.out.println(a >= b);
+        //System.out.println(a <= b);
+
+        // 4. Logical Operators
+        //System.out.println(9>7 && 9<7);
+        //System.out.println(9>7 || 9<7);
+    }
+}
