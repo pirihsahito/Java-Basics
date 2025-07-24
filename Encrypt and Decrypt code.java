2c9905d6cf9ee1164a9eaa69db355bf3a110a36a
Pirih Sahito <pirihsahito786@gmail.com>
2025-07-24 16:28:59 +0500

    Encrypt and Decrypt code.java

--- Encrypt and Decrypt code.java
+++ Encrypt and Decrypt code.java
@@ -1,6 +1,7 @@
 public class EncryptDecrypt {
     public static void main(String[] args) {
         char grade = 'B';
+        // Encrypting Grade
         grade = (char) (grade + 8);
         System.out.println(grade);
         // Decrypting Grade
