4dae0bae87b8fcd1786f31002a1aefc99ea7af1a
Pirih Sahito <pirihsahito786@gmail.com>
2025-07-24 15:39:03 +0500

    Create Literals in Java.java

---
+++ Literals in Java.java
@@ -0,0 +1,24 @@
+public class Literals {
+    public static void main(String[] args) {
+        // write your code here
+        byte age = 34;
+        int age2 = 65;
+        short age3 = 87;
+        long age4 = 56666666666666L;
+        char ch = 'A';
+        float f1 = 5.6f;
+        double d1 = 9.99;
+        // by default decimal number is considered as double literal
+        boolean a = true;
+        String str = "PS";
+        System.out.println(age);
+        System.out.println(age2);
+        System.out.println(age3);
+        System.out.println(age4);
+        System.out.println(ch);
+        System.out.println(f1);
+        System.out.println(d1);
+        System.out.println(a);
+        System.out.println(str);
+    }
+}
