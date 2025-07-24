18eaed4df4ed8fd926d9cd15c0d8f1d1dab2cf51
Pirih Sahito <pirihsahito786@gmail.com>
2025-07-24 16:35:42 +0500

    Math Expression code.java

---
+++ Math Expression code.java
@@ -0,0 +1,14 @@
+public class Expression {
+    public static void main(String[] args) {
+        // Expression 1
+        int a = 2;
+        int s = 3;
+        int u = 4;
+        int v = 5;
+        float e1 = (float) (v*v - u*u)/2*a*s;
+        System.out.println(e1);
+        // Expression 2
+        int e2 = 7 * 49 / 7 + 35 / 7;
+        System.out.println(e2);
+    }
+}
