cc462d38c3cb4afaca07dee36c129e2587c93fd7
Pirih Sahito <pirihsahito786@gmail.com>
2025-07-24 15:56:36 +0500

    Create Calculate CGPA in Java.java

---
+++ Calculate CGPA in Java.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int subject1 = 75;
+        int subject2 = 87;
+        int subject3 = 72;
+        int total = subject1 + subject2 + subject3;
+        float cgpa = (float) total/30;
+        System.out.println("CGPA =");
+        System.out.println(cgpa);
+    }
+}
