1b6b23d215909cd046a58f8da09c3c2c1e7c0499
Pirih Sahito <pirihsahito786@gmail.com>
2025-07-24 15:48:14 +0500

    Create Int Variable in Java.java

---
+++ Int Variable in Java.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+// write your code here
+        System.out.print("The sum is: ");
+        int num1 = 1;
+        int num2 = 2;
+        int num3 = 3;
+        int sum = num1 + num2 + num3;
+        System.out.println(sum);
+    }
+}
+
+// add two numbers
+// AddTwoNumbers --> pascal naming convention
+// addTwoNumbers ---> camel case naming convention
