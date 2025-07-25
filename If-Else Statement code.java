521e280872443805d4f69af7a4b64c62162a1ae7
Pirih Sahito <pirihsahito786@gmail.com>
2025-07-25 14:13:30 +0500

    If-Else Statement code.java

---
+++ If-Else Statement code.java
@@ -0,0 +1,18 @@
+public class Conditionals {
+    public static void main(String[] args) {
+        int age1 = 6; // Example 1
+        if(age1>18) {
+            System.out.println("Yes, you can derive...!");
+        }
+        else{
+            System.out.println("No, you can not drive yet...!");
+        }
+        int age2 = 26; //Example 2
+        if(age2>18) {
+            System.out.println("Yes, you can derive...!");
+        }
+        else{
+            System.out.println("No, you can not drive yet...!");
+        } // else is optional. If executes without else statement...
+    }
+}
