464eb0f90219eb72f87e1f8e68b5fa6ba51c601f
Pirih Sahito <pirihsahito786@gmail.com>
2025-07-26 11:35:05 +0500

    If-Else Statement code.java

--- If-Else Statement code.java
+++ If-Else Statement code.java
@@ -1,5 +1,7 @@
 public class Conditionals {
     public static void main(String[] args) {
+
+        // For Relational Operators
         int age1 = 6; // Example 1
         if(age1>18) {
             System.out.println("Yes, you can derive...!");
@@ -14,5 +16,39 @@ public class Conditionals {
         else{
             System.out.println("No, you can not drive yet...!");
         } // else is optional. If executes without else statement...
-    }
+        int age3 = 36; // Example 3
+        if(age3==18) {
+            System.out.println("Yes, you can derive...!");
+        }
+        else{
+            System.out.println("No, you can not drive yet...!");
+        }
+        // For Logical Operators
+        System.out.println("For Logical AND");
+        boolean b1 = false;
+        boolean b2 = false;
+        boolean b3 = false;
+        if(b1 && b2 && b3){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        } // Evaluates to true when at least all conditions are true...
+
+        System.out.println("For Logical OR");
+        boolean a = true;
+        boolean b = false;
+        if(a || b){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }  // Evaluates to true when at least 1 of conditions is true...
+
+        System.out.println("For Logical NOT");
+        System.out.print("Not(a) is ");
+        System.out.println(!a);
+        System.out.print("Not(b) is ");
+        System.out.println(!b);
+    } // Logical NOT is also known as Inverter. True becomes false and false become true.
 }
