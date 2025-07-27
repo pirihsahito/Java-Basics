48dd5fc14136a9f5145d898aad5b2b74f4543b12
Pirih Sahito <pirihsahito786@gmail.com>
2025-07-27 11:53:12 +0500

    Find Out Leap Year code.java

--- Find Out Leap Year code.java
+++ Find Out Leap Year code.java
@@ -5,11 +5,11 @@ public class LeapYear {
         System.out.println("Enter Year :");
         Scanner sc = new Scanner(System.in);
         int year = sc.nextInt();
-        if(year % 4 == 0){
-            System.out.println("This Is A Leap Year...!!!");
+        if((year % 4 == 0 && year % 100 != 0) || (year % 100 == 0 && year % 400 == 0)){
+            System.out.println("This is a Leap Year...!!!");
         }
         else{
-            System.out.println("This Is Not A Leap Year...!!!");
+            System.out.println("This is not a Leap Year...!!!");
         }
     }
 }
