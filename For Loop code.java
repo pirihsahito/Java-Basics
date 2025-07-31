6e56b0eadcbf3a5048a634885863e2c0101a651a
Pirih Sahito <pirihsahito786@gmail.com>
2025-07-31 11:07:16 +0500

    For Loop code.java

---
+++ For Loop code.java
@@ -0,0 +1,21 @@
+public class Loops {
+    public static void main(String[] args) {
+        System.out.println("For Loop in Java :");
+
+        // Example 1
+        for(int d = 1; d<=15; d++){
+            System.out.println(d);
+        }
+
+        //  Example 2
+        int n = 5;
+        for(int e = 0; e<n; e++){
+            System.out.println(2*e+1);
+        }
+
+        //  Example 3
+        for(int f = 10; f>0; f--){
+            System.out.println(f);
+        }
+    }
+}
