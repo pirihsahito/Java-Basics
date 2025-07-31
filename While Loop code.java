05ee3d23fb9631f5a2a3b50470291759303d027d
Pirih Sahito <pirihsahito786@gmail.com>
2025-07-31 10:58:35 +0500

    While Loop code.java

---
+++ While Loop code.java
@@ -0,0 +1,14 @@
+public class Loops {
+    public static void main(String[] args) {
+        System.out.println("While Loop in Java :");
+
+        int a = 1;
+        while(a<= 100){
+            System.out.println(a);
+            a++;
+        }
+        /* while(true){
+            System.out.println("Infinite While Loop");
+        }*/
+    }
+}
