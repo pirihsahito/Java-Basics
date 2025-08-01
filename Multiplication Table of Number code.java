f4be4da092e211814c5f5da13e3f0226bc077183
Pirih Sahito <pirihsahito786@gmail.com>
2025-08-01 17:32:05 +0500

    Multiplication Table of Number code.java

---
+++ Multiplication Table of Number code.java
@@ -0,0 +1,9 @@
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        System.out.println("Table of 9 :");
+        int n = 9;
+        for(int i = 1; i <= 10; i++){
+            System.out.printf("%d x %d = %d\n", n, i, n*i);
+        }
+    }
+}
