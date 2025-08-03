8f0b0216871d4f9fa2cecb99c7a8520259c36871
Pirih Sahito <pirihsahito786@gmail.com>
2025-08-03 17:16:51 +0500

    Multi-Dimensional Arrays code.java

---
+++ Multi-Dimensional Arrays code.java
@@ -0,0 +1,21 @@
+public class MultiDimensionalArray {
+    public static void main(String[] args) {
+        // 2-D Array
+        int [][] flats;
+        flats = new int[2][3];
+        flats[0][0] = 100;
+        flats[0][1] = 101;
+        flats[0][2] = 102;
+        flats[1][0] = 200;
+        flats[1][1] = 201;
+        flats[1][2] = 202;
+        System.out.println("Displaying 2-D array Using For Loop :");
+        for(int i=0; i<flats.length; i++){
+            for(int j=0; j<flats[i].length; j++){
+                System.out.print(flats[i][j]);
+                System.out.print(" ");
+            }
+            System.out.println("");
+        }
+    }
+}
