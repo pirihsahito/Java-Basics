1056fc5c6b373a06dd0303a5a5488153bb663b7d
Pirih Sahito <pirihsahito786@gmail.com>
2025-08-03 15:38:34 +0500

    Array Length code.java

---
+++ Array Length code.java
@@ -0,0 +1,8 @@
+public class ArrayLength {
+        public static void main(String[] args) {
+
+            int [] marks = {98, 87, 68, 90, 79};
+            // Length of an Array...
+            System.out.println(marks.length);
+        }
+    }
