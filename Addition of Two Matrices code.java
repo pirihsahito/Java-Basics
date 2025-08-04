08a4ca244f66b2eda688f18b43eddd14620cb168
Pirih Sahito <pirihsahito786@gmail.com>
2025-08-04 12:46:40 +0500

    Addition of Two Matrices code.java

---
+++ Addition of Two Matrices code.java
@@ -0,0 +1,21 @@
+public class MatricesAddition {
+    public static void main(String[] args) {
+        int [][] matrix1 = {{1,3,5},
+                           {7,9,11}};
+
+        int [][] matrix2 = {{2,4,6},
+                           {8,10,12}};
+
+        int [][] result = {{0,0,0},
+                          {0,0,0}};
+
+        System.out.println("Addition of Two Matrices :");
+        for(int i = 0; i<matrix1.length; i++){
+            for(int j = 0; j<matrix1[i].length; j++){
+                result[i][j] = matrix1[i][j] + matrix2[i][j];
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println(""); // Prints a new line...
+        }
+    }
+}
