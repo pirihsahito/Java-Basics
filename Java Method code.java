8aa52c455c1e7bb1f27ff85a95d71c745623c521
Pirih Sahito <pirihsahito786@gmail.com>
2025-08-10 16:49:32 +0500

    Java Method code.java

---
+++ Java Method code.java
@@ -0,0 +1,25 @@
+public class JavaMethods {
+    static int method(int x, int y){
+        int z;
+        if(x>y){
+            z = x+y;
+        }
+        else{
+            z = (x + y)*5;
+        }
+        return z;
+    }
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        int c;
+        c = method(a, b);
+        int a1 = 2;
+        int b1 = 1;
+        int c1;
+        c1 = method(a1, b1);
+        System.out.println(c);
+        System.out.println(c1);
+    }
+}
