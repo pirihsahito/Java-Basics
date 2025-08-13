79547c96f1092a89ad17fba2f17548c59a1d8a8e
Pirih Sahito <pirihsahito786@gmail.com>
2025-08-13 18:04:04 +0500

    Void Return Type code.java

---
+++ Void Return Type code.java
@@ -0,0 +1,9 @@
+public class VoidReturnType {
+    static void joke(){
+        System.out.println("I have a joke on coding but it will not work on your computer...");
+    }
+
+    public static void main(String[] args) {
+        joke();
+    }
+}
